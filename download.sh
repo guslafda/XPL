2d002f3768e7a98984a6df6b9d279f887d5e75ea
guslafda <129531754+guslafda@users.noreply.github.com>
2025-05-14 14:51:55 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-LINK_ID="1BqT-PTSJD1Id0BHsffuUt_UGbYzig2C_"
+FILE_ID="1BqT-PTSJD1Id0BHsffuUt_UGbYzig2C_"
 OUTPUT_FILE="xpl.tar"
 
-CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$LINK_ID" -O- | sed -rn 's/.*name="confirm" value="([0-9A-Za-z_]+)".*/\1\n/p')
-UUID=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$LINK_ID" -O- | sed -rn 's/.*name="uuid" value="([0-9A-Za-z_-]+)".*/\1\n/p')
-
-wget --no-check-certificate --load-cookies /tmp/cookies.txt \
-"https://drive.usercontent.google.com/download?export=download&id=$LINK_ID&confirm=$CONFIRM&uuid=$UUID" \
--O $OUTPUT_FILE && rm -f /tmp/cookies.txt
+curl -L -o $OUTPUT_FILE "https://drive.google.com/uc?export=download&id=${FILE_ID}"
 
 mkdir -p models
 mv $OUTPUT_FILE models/
