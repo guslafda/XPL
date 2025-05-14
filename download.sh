c2edae552717402382832b10bda5c519e4d29429
guslafda <129531754+guslafda@users.noreply.github.com>
2025-05-14 15:26:58 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-FILE_ID="1BqT-PTSJD1Id0BHsffuUt_UGbYzig2C_"
-FILE_NAME="xpl.tar"
+$FILE_ID="1BqT-PTSJD1Id0BHsffuUt_UGbYzig2C_"
+$FILE_NAME="xpl.tar"
 
-CONFIRM=$(curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${FILE_ID}" | \
-grep -o 'confirm=[^&]*' | sed 's/confirm=//')
+gdown --id $FILE_ID -O $FILE_NAME
 
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CONFIRM}&id=${FILE_ID}" -o "${FILE_NAME}"
-
-mkdir -p models
-mv "${FILE_NAME}" models/
-cd models
-tar -xf "${FILE_NAME}"
-rm "${FILE_NAME}"
+mkdir models
+Move-Item $FILE_NAME models
+Set-Location models
+tar -xf $FILE_NAME
+Remove-Item $FILE_NAME
