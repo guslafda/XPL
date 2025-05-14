c39d8181bcfd865e12f33b5d5f3b9ade1be75d8a
guslafda <129531754+guslafda@users.noreply.github.com>
2025-05-14 15:30:50 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-$FILE_ID="1BqT-PTSJD1Id0BHsffuUt_UGbYzig2C_"
-$FILE_NAME="xpl.tar"
+FILE_ID="1BqT-PTSJD1Id0BHsffuUt_UGbYzig2C_"
+FILE_NAME="xpl.tar"
 
 gdown --id $FILE_ID -O $FILE_NAME
 
-mkdir models
-Move-Item $FILE_NAME models
-Set-Location models
+mkdir -p models
+mv $FILE_NAME models/
+cd models
 tar -xf $FILE_NAME
-Remove-Item $FILE_NAME
+rm $FILE_NAME
